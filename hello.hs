750e3783064a7b9b21a34d96b997b0607e14385f
Grover Lancaster-Cole <glancastercole@me.com>
2024-07-28 21:01:34 +1000

    finished section 3.5

--- hello.hs
+++ hello.hs
@@ -1,5 +1,7 @@
 -- hello.hs
 
+import Html
+
 main :: IO ()
 main = putStrLn (render myhtml)
 
@@ -14,42 +16,3 @@ myhtml =
                 (p_ "I suppose I am also learning some HTML.")
             )
         )
-
-newtype Html = Html String
-
-newtype Structure = Structure String
-
-type Title = String
-
-html_ :: Title -> Structure -> Html
-html_ title content =
-    Html (
-        el "html" (
-            (el "head" (el "title" title))
-            <> el "body" (getStructureString content)
-        )
-    )
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-el :: String -> String -> String
-el tag content =
-   "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-append_ :: Structure -> Structure -> Structure
-append_ strc1 strc2 =
-    Structure ( getStructureString strc1  <> getStructureString strc2)
-
-getStructureString :: Structure -> String
-getStructureString content =
-    case content of
-        Structure str -> str
-
-render :: Html -> String
-render html =
-    case html of
-        Html str -> str

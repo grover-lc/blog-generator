178015060e9a038824f3a9d336690969f4d5ff8c
Grover Lancaster-Cole <glancastercole@me.com>
2024-09-18 20:03:00 +1000

    arranged into cabal project

--- src/HsBlog/Convert.hs
+++ src/HsBlog/Convert.hs
@@ -1,9 +1,9 @@
 -- Convert.hs
 
-module Convert where
+module HsBlog.Convert where
 
-import qualified Markup
-import qualified Html
+import qualified HsBlog.Markup as Markup
+import qualified HsBlog.Html as Html
 
 
 convert :: Html.Title -> Markup.Document -> Html.Html
